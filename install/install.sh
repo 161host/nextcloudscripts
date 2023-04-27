b2ac78c8b3bddca6eb20ecccbcb12ed5bc65d10a
Thies Mueller <thies.mueller@gec.io>
2023-04-27 13:59:26 +0200

    feat: list active instances

--- install/install.sh
+++ install/install.sh
@@ -8,34 +8,44 @@
 
 # This is just another excuse to not write an ansible role cause it is soo much easier for you to setup. (Yes I'll show myself to the door....)
 
+# the whole setup assumes that the user you're using is root. If this isn't the case, well. Search for another script or alter it the way you want. (If it comes out nice, feel free to create a MR)
 if [[ $EUID -ne 0 ]]; then
    echo "This script must be run as root" 
    exit 1
 fi
 
-
+# Update & Upgrade the System
 apt update && apt upgrade -y
+
+# Install all the stuff you need
 apt install nginx certbot wget unzip bash sed python3-certbot-nginx mariadb-server php8.1 php8.1-cli php8.1-fpm php8.1-zip php8.1-mysql php8.1-opcache php8.1-mbstring php8.1-xml php8.1-gd php8.1-curl libmagickcore-6.q16-3-extra php8.1-imagick -y
 
+# copy the nginx config snippets that we need
 cp /scripts/install/src/immutable.conf /etc/nginx/conf.d/immutable.conf
 cp /scripts/install/src/php-handler.conf /etc/nginx/conf.d/php-handler.conf
 
+# test the nginx config
 nginx -t
 
+# create this directory
 mkdir /scripts/data
 
+# create the ncmgmt command and source the .bashrc to have it ready
 echo 'alias ncmgmt="bash /scripts/domainmgmt.sh"' >> ~/.bashrc
 source ~/.bashrc
 
+# enter your custom subdomain. If you don't use that part this is the point where you can Ctrl+C out of this shit.
 read -p "Enter your custom subdomain:  " subdomain
 
-
+# copy the templates into the production paths
 cp /scripts/install/src/subdomain.tmpl /scripts/templates/subdomain
 cp /scripts/install/src/creation_sub.sh.tmpl /scripts/creation_sub.sh
 
+# replace the template with the production values
 sed -i "s/replacewithactualsubdomain/$subdomain/g" /scripts/creation_sub.sh
 sed -i "s/replacewithactualsubdomain/$subdomain/g" /scripts/templates/subdomain
 
+# a little note regarding the wildcard certificate
 echo "I'll just expect that your wildcard certificate is here: /etc/letsencrypt/live/$subdomain/"
 echo " "
 echo "If not. You wanna change that in /scripts/templates/subdomain"
