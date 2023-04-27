6fd61a8cce0fb15631d1fc9581e13795edef5333
Thies Mueller <thies.mueller@gec.io>
2023-04-27 13:59:40 +0200

    feat: list active nextcloud instances

---
+++ listdomains.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+###################################################
+# Author: Thies Mueller (https://thiesmueller.de) #
+# License: GNU AGPL v3.0                          #
+# USE AT YOUR OWN RISK!                           #
+# Originally developed for 161host.net            #
+###################################################
+
+ls -A1 /etc/nginx/sites-enabled/
